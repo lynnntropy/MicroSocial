0c10567a66a4ad3f85d9e9c8957cb751d607c1ec
Veselin <omegavesko@gmail.com>
2015-03-08 23:16:21 +0100

    Added basic code to the server to connect o and use a local SQLite database. Currently only the Users table has been implemented.

---
+++ MicroSocialServer/MicroSocialServer/DatabaseManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SQLite;
+
+namespace MicroSocialServer
+{
+    
+    class DatabaseManager
+    {
+        static string databaseFilename = "database.db";
+        SQLiteConnection databaseConnection;
+
+        public DatabaseManager()
+        {
+
+        }
+
+        public void Connect()
+        {
+            this.databaseConnection = new SQLiteConnection("Data Source=" + databaseFilename + ";Version=3;");
+            this.databaseConnection.Open();
+        }
+
+        public void Close()
+        {
+            if (this.databaseConnection != null)
+            {
+                this.databaseConnection.Close();
+            }
+        }
+
+        public void AddUser(string username, string passwordHash)
+        {
+            string sqlCommand = String.Format(
+                "INSERT INTO Users VALUES ('{0}', '{1}');",
+                username, passwordHash
+                );
+
+            SQLiteCommand command = new SQLiteCommand(sqlCommand, databaseConnection);
+            int rowsAffected = command.ExecuteNonQuery();
+        }
+    }
+}
--- MicroSocialServer/MicroSocialServer/ServerResource.cs
+++ MicroSocialServer/MicroSocialServer/ServerResource.cs
@@ -8,15 +8,54 @@ using Grapevine;
 using Grapevine.Server;
 using System.Net;
 
+using Newtonsoft.Json.Linq;
+
 
 namespace MicroSocialServer
 {
     public sealed class ServerResource : RESTResource
     {
         [RESTRoute(Method = HttpMethod.GET, PathInfo = @"^/hello")]
-        public void HandleFooBarRequests(HttpListenerContext context)
-        {            
-            this.SendTextResponse(context, String.Format("Hello World! It's {0}!", DateTime.Now));
+        public void SayHello(HttpListenerContext context)
+        {
+            //this.SendTextResponse(context, String.Format("Hello World! It's {0} here!", DateTime.Now.TimeOfDay));
+
+            Console.WriteLine(context.Request.ToString());
+
+            //this.SendTextResponse(context, String.Format("Hello World! It's {0} here!", DateTime.Now.TimeOfDay));
+            this.SendTextResponse(context, context.Request.QueryString.ToString());
+        }
+
+        [RESTRoute(Method = HttpMethod.POST, PathInfo = @"^/helloPost")]
+        public void SayHelloPost(HttpListenerContext context)
+        {
+            JObject jsonPayload = GetJsonPayload(context.Request);
+
+            if (jsonPayload != null) this.SendTextResponse(context, jsonPayload.ToString());
+            else this.SendTextResponse(context, "Payload is null.");
+        }
+
+        [RESTRoute(Method = HttpMethod.POST, PathInfo = @"^/register")]
+        public void RegisterUser(HttpListenerContext context)
+        {
+            JObject jsonPayload = GetJsonPayload(context.Request);
+
+            String username = jsonPayload.GetValue("username").ToString();
+            String passwordHash = jsonPayload.GetValue("passwordHash").ToString();
+
+            if (username != null && passwordHash != null)
+            {
+                DatabaseManager dbManager = new DatabaseManager();
+                dbManager.Connect();
+                dbManager.AddUser(username, passwordHash);
+                dbManager.Close();
+
+                this.SendTextResponse(context, String.Format("Successfully registered user {0}.", username));
+            }
+            else
+            { 
+                this.SendTextResponse(context, "No valid user received.");
+            }
         }
     }
 }
